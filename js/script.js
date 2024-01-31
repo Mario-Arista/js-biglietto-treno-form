8d9f224087f956e185265f403c5af2e69e5c7d1a
mario.arista <mario.arista.dev@gmail.com>
2024-01-31 17:37:53 +0100

    still doesn't work

--- js/script.js
+++ js/script.js
@@ -8,60 +8,58 @@ const inputAgeElement = document.querySelector("#choice-age-field");
 const buttonGeneraElement = document.querySelector("#genera");
 const buttonAnnullaElement = document.querySelector("#annulla");
 
+// OUTPUT
+const outputNameElement = document.querySelector("#output-name");
+const outputTicketElement = document.querySelector("#type_ticket");
+const outputCarrozzaElement = document.querySelector("#carrozza-random");
+const outputCodiceElement = document.querySelector("#carrozza-random");
+const outputPriceElement = document.querySelector("#out-price");
+
 // PRICE PER KM 
 
 let pricePerKm;
 
 // RANDOM NUMBERS
 const sceltaComputerCarrozza = Math.floor(Math.random() * 15) + 1;
-console.log(sceltaComputerCarrozza);
 
 const sceltaComputerCPM = Math.floor(Math.random() * 9999) + 90000;
-console.log(sceltaComputerCPM);
-
-
-
-
-/* 
-Scrivere un programma che chieda all’utente:
- - Il numero di chilometri da percorrere
- - Età del passeggero
-
- Sulla base di queste informazioni dovrà calcolare il prezzo 
- totale del biglietto di viaggio, secondo le seguenti regole:
- - il prezzo del biglietto è definito in base ai km (0.21 € al km)
- - va applicato uno sconto del 20% per i minorenni
- - va applicato uno sconto del 40% per gli over 65.
-*/
-
 
 
 buttonGeneraElement.addEventListener("click",
     function() {
 
-        // al click sul bottone genera, si generano gli output:
+        // al click sul bottone "GENERA", si generano gli output:
 
         // NOME
-        document.getElementById("output-name").innerHTML = inputFullNameElement.value;
+        outputNameElement.innerHTML = inputFullNameElement.value;
 
         // NUMERO RANDOM CARROZZA
-        document.getElementById("carrozza-random").innerHTML = sceltaComputerCarrozza;
+        outputCarrozzaElement.innerHTML = sceltaComputerCarrozza;
 
         // NUMERO RANDOM CODICE
-        document.getElementById("codice-random").innerHTML = sceltaComputerCPM;
+        outputCodiceElement.innerHTML = sceltaComputerCPM;
 
         // PREZZO PER KM + AGE CHOICE 
 
-        if (inputAgeElement.value == "Minorenne") {
-            document.getElementById("type_ticket").innerHTML = "Biglietto scontato del 20%";
-            document.getElementById("out-price").innerHTML = (inputKmElement.value * 0.21 * 0.80);
-        } else if (inputAgeElement.value == "Over 65") {
-            document.getElementById("type_ticket").innerHTML = "Biglietto scontato del 40%";
-            document.getElementById("out-price").innerHTML = (inputKmElement.value * 0.21 * 0.60);
+        pricePerKm = inputKmElement.value * 0.21;
+
+        if (inputAgeElement.value === "Minorenne") {
+
+            outputTicketElement.innerHTML = "Biglietto scontato del 20%";
+            outputPriceElement.innerHTML = pricePerKm * 0.80 + "€";
+
+
+        } else if (inputAgeElement.value === "Over 65") {
+
+            outputTicketElement.innerHTML = "Biglietto scontato del 40%";
+            outputPriceElement.innerHTML = pricePerKm * 0.60 + "€";
+
 
         } else {
-            document.getElementById("type_ticket").innerHTML = "Biglietto standard";
-            document.getElementById("out-price").innerHTML = (inputKmElement.value * 0.21);
+
+            outputTicketElement.innerHTML = "Biglietto standard";
+            outputPriceElement.innerHTML = pricePerKm;
+
         }
 
     }
